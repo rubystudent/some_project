2e94db8b3e8159e2413e27f6b6ea4c6a91657d43
rubystudent <cfgcl18@ukr.net>
2016-10-28 15:43:11 +0300

    Added bootstrap

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def current_user
+    @current_user = User.find_by(token: session[:current_user_id])
+  end
+
+  helper_method :current_user
 end
