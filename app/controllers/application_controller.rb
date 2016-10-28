b3998c147311272074bd69c29594d2eb5b20c9b3
rubystudent <cfgcl18@ukr.net>
2016-10-28 16:34:45 +0300

    Added dashboard controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,9 @@ class ApplicationController < ActionController::Base
     @current_user = User.find_by(token: session[:current_user_id])
   end
 
+  def require_user
+    redirect_to new_session_path unless current_user
+  end
+
   helper_method :current_user
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
 
-  root "users#show"
+  root "dashboard#show"
 
   resource :session, only:[:new, :create, :destroy]
-  resources :users
+  resources :users do 
+    resource :dashboard, only:[:show]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
